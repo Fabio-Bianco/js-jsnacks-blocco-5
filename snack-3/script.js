dc3b1686e9e7fc96e00eb6eb78324618a6af8e22
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-12 19:45:01 +0100

    Ex 03: solved with Map

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,23 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+
+console.log(numbers);
+
+console.log('==========')
+
+//versione estesa
+
+const NumbersNew = numbers.map(function(num){
+    return num + 1;
+})
+
+console.log(NumbersNew);
+
+console.log('==========')
+
+// arrow function
+const newNumbers = numbers.map(num => num +1);
+
+console.log(newNumbers);
