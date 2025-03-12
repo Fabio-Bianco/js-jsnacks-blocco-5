ee3a67bfd3ddb5a5a33f89228853ac627355266c
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-12 19:24:04 +0100

    EX 2: solved with forEach

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,3 +6,24 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+
+// Versione estesa
+people.forEach(function(person){
+  console.log(person.name)
+})
+
+console.log ('=========')
+
+// Versione con arrow function
+people.forEach(person => {
+  console.log(person.name)
+})
+
+console.log ('=========')
+
+// Versione con ciclo for
+
+for(let i = 0; i < people.length; i++){
+  console.log(people[i].name)
+}
