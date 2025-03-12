7f87a038e7021dd938da3ff60a622cebc71d209a
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-12 19:14:06 +0100

    EX 1: solved with forEach

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,25 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+// Versione estesa
+names.forEach(function(name){
+    console.log(name)
+})
+
+console.log ('=========')
+
+// Versione con arrow function
+names.forEach(name => {
+    console.log(name);
+});
+
+console.log ('=========')
+
+
+// Versione con ciclo for
+
+for(let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+    
+}
